5fc76776db9f9485fff4c9c2fe159f2a4cf2cac1
shraddha-singh <shraddha-singh@ibm.com>
2020-05-11 17:02:55 -0500

    add agg function

--- custom/functions.py
+++ custom/functions.py
@@ -62,3 +62,34 @@ class SS_HelloWorld(BaseTransformer):
         outputs = [
             ui.UIFunctionOutSingle(name='greeting_col', datatype=str, description='Output item produced by function')]
         return (inputs, outputs)
+
+
+class SS_HelloWorldAggregator(BaseSimpleAggregator):
+    '''
+    The docstring of the function will show as the function description in the UI.
+    '''
+
+    def __init__(self, source=None, expression=None):
+        if expression is None or not isinstance(expression, str):
+            raise RuntimeError("argument expression must be provided and must be a string")
+
+        self.input_items = source
+        self.expression = expression
+
+    def execute(self, group):
+        return eval(re.sub(r"\$\{GROUP\}", r"group", self.expression))
+
+    @classmethod
+    def build_ui(cls):
+        inputs = []
+        # Input variable name must be kept 'source'
+        # Output variable name must be kept 'name'
+        inputs.append(UIMultiItem(name='source', datatype=None, description=('Choose the data items'
+                                                                            ' that you would like to'
+                                                                                  ' aggregate'),
+                                  output_item='name', is_output_datatype_derived=True))
+
+        inputs.append(UIExpression(name='expression', description='Use ${GROUP} to reference the current grain.'
+                                                    'All Pandas Series methods can be used on the grain.'
+                                                    'For example, ${GROUP}.max() - ${GROUP}.min().'))
+        return (inputs, [])
