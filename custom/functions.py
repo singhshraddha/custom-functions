a9fc51e8cbbc4c70ffbeef941e3d9ca573bb913c
shraddha-singh <shraddha-singh@ibm.com>
2020-05-11 17:44:59 -0500

    add agg function

--- custom/functions.py
+++ custom/functions.py
@@ -5,6 +5,7 @@ import math
 from sqlalchemy.sql.sqltypes import TIMESTAMP, VARCHAR
 import numpy as np
 import pandas as pd
+import re
 
 from iotfunctions.base import (BaseTransformer,
                                BaseSimpleAggregator)
