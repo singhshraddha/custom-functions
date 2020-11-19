df4446c2c90e3343d6a9c76dbbd05a40f9a85e94
shraddha-singh <shraddha-singh@ibm.com>
2020-11-19 14:54:30 -0600

    trying: variable output columns

--- scripts/register_function.py
+++ scripts/register_function.py
@@ -7,10 +7,12 @@ from iotfunctions.enginelog import EngineLogging
 EngineLogging.configure_console_logging(logging.DEBUG)
 
 with open('../dev_resources/credentials_as_dev.json', encoding='utf-8') as F:
+#with open('../dev_resources/cognio.json', encoding='utf-8') as F:
     credentials = json.loads(F.read())
 db_schema = None
 db = Database(credentials=credentials)
 
-from custom.forecast import Cognio_NeuralNetwork_Forecaster
+#from custom.forecast import Cognio_NeuralNetwork_Forecaster
+from custom.functions import SS_HelloWorld
 
-db.register_functions([Cognio_NeuralNetwork_Forecaster])
+db.register_functions([SS_HelloWorld])
