07f99a0c039b72ec5bd8125a899b5350124c9fa7
shraddha-singh <shraddha-singh@ibm.com>
2020-12-04 14:34:14 -0600

    testing matrix profile

--- custom/unsupervised_anomaly.py
+++ custom/unsupervised_anomaly.py
@@ -93,7 +93,6 @@ class MatrixProfileAnomalyScoreTest(BaseTransformer):
         # check data type
         if df_copy[self.input_item].dtype != np.float64:
             return df_copy
-        logger.debug(f'Df copy data types {df_copy.dtypes}')
 
         for entity in entities:
             # per entity - copy for later inplace operations
@@ -109,9 +108,8 @@ class MatrixProfileAnomalyScoreTest(BaseTransformer):
 
             # minimal time delta for merging
             mindelta, dfe_orig = min_delta(dfe_orig)
-            logger.debug(f'Dfe data types {dfe.dtypes}')
 
-            matrix_profile = stumpy.aamp(dfe[self.input_item], m=self.window_size)[:, 0]
+            matrix_profile = stumpy.stump(dfe[self.input_item], m=self.window_size)[:, 0]
             # fill in small value for newer data points with < window_size num data points following them
             fillers = np.array([self.NOT_ENOUGH_DATAPOINTS] * (self.window_size - 1))
             matrix_profile = np.append(matrix_profile, fillers)
