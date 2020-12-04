6cd67b0cc295f6cf654d68ce11641b81082bd7f0
shraddha-singh <shraddha-singh@ibm.com>
2020-12-04 14:46:34 -0600

    testing matrix profile

--- custom/unsupervised_anomaly.py
+++ custom/unsupervised_anomaly.py
@@ -72,7 +72,7 @@ class MatrixProfileAnomalyScoreTest(BaseTransformer):
     """
     TODO: add description
     """
-    NOT_ENOUGH_DATAPOINTS = 1e-15
+    DATAPOINTS_AFTER_LAST_WINDOW = 1e-15
     INIT_SCORES = 1e-20
 
     def __init__(self, input_item, output_item, window_size=12):
@@ -109,9 +109,9 @@ class MatrixProfileAnomalyScoreTest(BaseTransformer):
             # minimal time delta for merging
             mindelta, dfe_orig = min_delta(dfe_orig)
 
-            matrix_profile = stumpy.aamp(dfe[self.input_item], m=self.window_size)[:, 0]
+            matrix_profile = stumpy.aamp(dfe[[self.input_item]].fillna(0).to_numpy(dtype=np.float64), m=self.window_size)[:, 0]
             # fill in small value for newer data points with < window_size num data points following them
-            fillers = np.array([self.NOT_ENOUGH_DATAPOINTS] * (self.window_size - 1))
+            fillers = np.array([self.DATAPOINTS_AFTER_LAST_WINDOW] * (self.window_size - 1))
             matrix_profile = np.append(matrix_profile, fillers)
 
             anomaly_score = merge_score(dfe, dfe_orig, self.output_item, matrix_profile, mindelta)
