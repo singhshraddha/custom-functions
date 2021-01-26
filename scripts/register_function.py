9f30795d642855a9db16fc5c2abdc347d4b7ec88
shraddha-singh <shraddha-singh@ibm.com>
2021-01-26 13:25:30 -0600

    iotls

--- scripts/register_function.py
+++ scripts/register_function.py
@@ -1,16 +1,14 @@
-#!/user/bin/env python3
 import json
 import logging
 from iotfunctions.db import Database
 from iotfunctions.enginelog import EngineLogging
-
 EngineLogging.configure_console_logging(logging.DEBUG)
 
-with open('../dev_resources/credentials_as_dev.json', encoding='utf-8') as F:
+with open('../dev_resources/credentials_iotls.json', encoding='utf-8') as F:
     credentials = json.loads(F.read())
 db_schema = None
 db = Database(credentials=credentials)
 
-from custom.unsupervised_anomaly import MatrixProfileAnomalyScoreTest
+from iotfunctions.anomaly import BayesRidgeRegressor
 
-db.register_functions([MatrixProfileAnomalyScoreTest])
+db.register_functions([BayesRidgeRegressor])
