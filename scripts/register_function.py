707fabb4c17cb73a5855406180d40be9c9af44f5
shraddha-singh <shraddha-singh@ibm.com>
2021-03-01 14:03:43 -0600

    model register function

--- scripts/register_function.py
+++ scripts/register_function.py
@@ -1,14 +1,14 @@
 import json
 import logging
 from iotfunctions.db import Database
-from iotfunctions.enginelog import EngineLogging
-EngineLogging.configure_console_logging(logging.DEBUG)
+#from iotfunctions.enginelog import EngineLogging
+#EngineLogging.configure_console_logging(logging.DEBUG)
 
-with open('../dev_resources/credentials_iotls.json', encoding='utf-8') as F:
+with open('../dev_resources/credentials_as_dev.json', encoding='utf-8') as F:
     credentials = json.loads(F.read())
 db_schema = None
 db = Database(credentials=credentials)
 
-from custom.supervised import BayesRidgeRegressor
+from custom.functions import SS_ComplexAggregator
 
-db.register_functions([BayesRidgeRegressor])
+db.register_functions([SS_ComplexAggregator])
--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        "iotfunctions @ git+https://github.com/ibm-watson-iot/functions.git@dev#egg=iotfunctions"
-        "pyrenn==0.1",
+        "iotfunctions @ git+https://github.com/ibm-watson-iot/functions.git@dev#egg=iotfunctions",
+        'pyrenn==0.1'
     ],
     extras_require = {
         'kafka':  ['confluent-kafka==0.11.5']
