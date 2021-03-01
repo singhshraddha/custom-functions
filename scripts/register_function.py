8117970db6859ec2daa13345768403184f01ea14
shraddha-singh <shraddha-singh@ibm.com>
2021-03-01 14:23:49 -0600

    model register function

--- scripts/register_function.py
+++ scripts/register_function.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from iotfunctions.db import Database
+
 #from iotfunctions.enginelog import EngineLogging
 #EngineLogging.configure_console_logging(logging.DEBUG)
 
@@ -11,4 +12,8 @@ db = Database(credentials=credentials)
 
 from custom.functions import SS_ComplexAggregator
 
+try:
+    db.unregister_functions(['SS_ComplexAggregator'])
+except:
+    pass
 db.register_functions([SS_ComplexAggregator])
