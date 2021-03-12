9579fe0da625a8b96527f915962852494f37d10b
shraddha-singh <shraddha-singh@ibm.com>
2021-03-12 14:33:09 -0600

    data quality

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        "iotfunctions @ git+https://github.com/ibm-watson-iot/functions.git@dev#egg=iotfunctions",
+        "iotfunctions",
         'pyrenn==0.1'
     ],
     extras_require = {
